3b3601c85ab235c4e760297b60caeffc834fe742
ReneFuentes26 <fuentesbenitezreneismael@gmail.com>
2023-04-19 20:34:38 -0600

    Correcciones

--- src/components/Contenidos/Jornalizacion.vue
+++ src/components/Contenidos/Jornalizacion.vue
@@ -6,7 +6,7 @@
         <div class="col">
         <h5>Semana 3:</h5>
         <h3>-Entrega de proyecto del laboratorio I: Fecha de entrega 31 de Enero. </h3>
-        <img src="./imagenes/actividad semana 3.png" width="850" height="250">
+        <img src="./imagenes/actividad_semana_3.png" width="850" height="250">
         </div>
         <br/>
         <br />
@@ -15,14 +15,14 @@
         <h5>Semana 4:</h5>
         <h3>-Entrega del Portafolio del Computo II: Se extendio el tiempo de entrega hasta el 26 de Febreo, la fecha orginial era 19 de Febrero. </h3>
         </div>
-        <img src="./imagenes/actividad semana 4.png" width="650" height="450">
+        <img src="./imagenes/actividad_semana_4.png" width="650" height="450">
         <br />
         <br />
 
         <div class="col">
         <h5>Semana 5:</h5>
         <h3>-Entrega de  las actividades practicas realizadas en clases: Fecha limite de entrega 19 de Febrero. </h3>
-        <img src="./imagenes/actividad semana 5.png" width="850" height="250">
+        <img src="./imagenes/actividad_semana_5.png" width="850" height="250">
         </div>
         <br />
         <br /> 
@@ -30,7 +30,7 @@
         <div>
         <h5>Semana 6:</h5>
         <h3>-Realizacion del Primer Examen Parcial teorico: Fecha limite de entrega 21 de Febrero en la hora de la clase practica Viernes 12:40-2:20 p.m</h3>
-        <img src="./imagenes/actividad parcial.png" width="650" height="450">
+        <img src="./imagenes/actividad_parcial.png" width="650" height="450">
         </div>
     </div>
 
--- src/components/Contenidos/Tareas.vue
+++ src/components/Contenidos/Tareas.vue
@@ -22,15 +22,15 @@
     <p>
         La otra tarea ex-aula del computo es la creación del Portafolio donde recopilamos todo lo aprendí y ponemos en practica dejando constancia de nuestras investigación a la hora de hacer nuestras tareas o practicas.
     </p>
-    <img src="./imagenes/Portafolio 1-2.png" width="650" height="450">
+    <img src="./imagenes/Portafolio_1-2.png" width="650" height="450">
     <br />
     <br />
     <p>
         Para este portafolio unicamente utilice una navbar ya que se me parecio lo más comodo pero este proyecto esta abierto para mucho más diseño.
     </p>
-    <img src="./imagenes/Portafolio 1-3.png" width="650" height="450">
-    <img src="./imagenes/Portafiolio 1-4.png" width="650" height="450">
-    <img src="./imagenes/Portafolio 1-5.png" width="650" height="450">
+    <img src="./imagenes/Portafolio_1-3.png" width="650" height="450">
+    <img src="./imagenes/Portafiolio_1-4.png" width="650" height="450">
+    <img src="./imagenes/Portafolio_1-5.png" width="650" height="450">
     
     <br />
     <br />
